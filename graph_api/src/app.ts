1278225685b69ecd2eb4d2b18a6c14cf3d46f7e5
Stuart Himmer <stuart.himmer@willowtreeapps.com>
2020-05-25 21:42:35 -0400

    added koa framework for middleware handling

--- graph_api/src/app.ts
+++ graph_api/src/app.ts
@@ -1,8 +1,13 @@
 // src/index.ts
 
 import "reflect-metadata";
+import Koa from 'koa';
+import KoaRouter from "koa-router";
+import bodyParser from 'koa-bodyparser';
+import logger from "koa-logger";
+import json from "koa-json";
 import { createConnection } from "typeorm";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from 'apollo-server-koa';
 import { buildSchema } from "type-graphql";
 import { CategoryResolver } from "./resolvers/CategoryResolver";
 import { Container } from "typedi";
@@ -11,17 +16,38 @@ import { UserResolver } from "./resolvers/UserResolver";
 import { ListingResolver } from "./resolvers/ListingResolver";
 
 async function main() {
+  const PORT = process.env.PORT || 3000;
+  const app = new Koa();
+  const router = new KoaRouter();
+
+  /** Middlewares */
+  app.use( json() );
+  app.use( logger() );
+  app.use( bodyParser() );
+
   const connection = await createConnection()
 
   Container.set("connection", connection);
+
   const schema = await buildSchema({
     resolvers: [CategoryResolver, LocationResolver, UserResolver, ListingResolver],
     container: Container,
     nullableByDefault: true
   });
+
   const server = new ApolloServer({ schema })
-  await server.listen(4000)
-  console.log("Server has started!")
+
+  /** Routes */
+  app.use( router.routes() ).use( router.allowedMethods() );
+
+  server.applyMiddleware({ app });
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+  app.listen({ port: PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+  )
+  
 }
 
 main().catch((e) => {
